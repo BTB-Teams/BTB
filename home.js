70c9a48ab5b15875d6e4bcab5b659d1d554ad47a
BTB-Teams <25.btb.teams.30@gmail.com>
2026-03-29 14:48:25 +0200

    commit

---
+++ home.js
@@ -0,0 +1,4 @@
+let today = new Date();
+document.writeln(today);
+
+alert("jambo kwako !, karibu kunako kurasa kamili la BTB ");
---
+++ test.php
@@ -0,0 +1,14 @@
+
+      <html>
+        <head>
+            <title>Test</title>
+        </head>
+        <body>
+              <?php
+             echo"welcome to php";
+             print("jambo");
+            phpinfo();
+             ?>
+         
+        </body>
+      </html>
